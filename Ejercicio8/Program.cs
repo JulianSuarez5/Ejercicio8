ac1b02eb4d269c8c4dc8df14c2d9cc3cd69e24e5
JulianSuarez5 <Juacosuarez12@gmail.com>
2023-09-04 22:35:07 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ConversionTemperatura
+{
+    static void Main()
+    {
+        double celsius, fahrenheit;
+
+        Console.Write("Ingrese una temperatura en grados Celsius: ");
+        celsius = double.Parse(Console.ReadLine());
+
+        fahrenheit = celsius * 9 / 5 + 32;
+
+        Console.WriteLine("La temperatura equivalente en grados Fahrenheit es: {0}", fahrenheit);
+
+        Console.ReadKey();
+    }
+}
